4c6e5102310ec88b5121be352fd5c3198158e2d3
Exonymus <exonymus@icloud.com>
2021-06-02 18:11:55 +0300

    Lab7

---
+++ Lab7/Converter.cs
@@ -0,0 +1,194 @@
+﻿using System;
+
+namespace Lab7
+{
+    class Converter : IComparable, IEquatable<Converter>
+    {
+        private int _num;
+        private int _denom;
+        private readonly bool changed = false;
+
+        public Converter(int _num, int _denom)
+        {
+            if (!changed)
+            {
+                int devide = GreatDivisor(_num, _denom);
+                this._denom = _denom / devide;
+                this._num = _num / devide;
+                changed = true;
+            }
+            else
+            {
+                this._denom = _denom;
+                this._num = _num;
+            }
+        }
+
+        public int GreatDivisor(int _num, int _denom)
+        {
+            if (_num % _denom == 0)
+                return _denom;
+
+            if (_denom % _num == 0)
+                return _num;
+
+            if (_num > _denom)
+                return GreatDivisor(_num % _denom, _denom);
+
+            return GreatDivisor(_num, _denom % _num);
+        }
+
+        public static Converter operator +(Converter num1, Converter num2)
+        {
+            int num = num1._num * num2._denom + num2._num * num1._denom;
+            int denom = num1._denom * num2._denom;
+
+            Converter result = new Converter(num, denom);
+
+            return result;
+        }
+
+        public static Converter operator -(Converter num1, Converter num2)
+        {
+            num2._num = (-num2._num);
+            Converter temp = num1 + num2;
+            num2._num = (-num2._num);
+
+            return temp;
+        }
+
+        public static Converter operator *(Converter num1, Converter num2)
+        {
+            int num = num1._num * num2._num;
+            int denom = num1._denom * num2._denom;
+
+            Converter result = new Converter(num, denom);
+
+            return result;
+        }
+
+        public static Converter operator /(Converter num1, Converter num2)
+        {
+            int num = num1._num * num2._denom;
+            int denom = num1._denom * num2._num;
+
+            Converter result = new Converter(num, denom);
+
+            return result;
+        }
+
+        public static bool operator <(Converter num1, Converter num2)
+        {
+            if (!num1.changed && !num2.changed)
+            {
+                num1._num *= num2._denom;
+                num2._num *= num1._denom;
+                num1._denom *= num2._denom;
+                num2._denom = num1._denom;
+                return num1._num < num2._num;
+            }
+
+            return num1._num < num2._num;
+
+        }
+
+        public static bool operator >(Converter num1, Converter num2)
+        {
+            if (!num1.changed && !num2.changed)
+            {
+                num1._num *= num2._denom;
+                num2._num *= num1._denom;
+                num1._denom *= num2._denom;
+                num2._denom = num1._denom;
+            }
+
+            return num1._num > num2._num;
+        }
+
+        public override string ToString()
+        {
+            string res = $"{_num}/{_denom}";
+
+            return res;
+        }
+
+        public double ToDouble()
+        {
+            double value = (double)_num / _denom;
+
+            return value;
+        }
+
+        public int CompareTo(object obj)
+        {
+            Converter another = obj as Converter;
+
+            if (another.ToDouble() - (double)_num / _denom < 2e-10)
+                return 1;
+
+            if ((double)_num / _denom - another.ToDouble() < 2e-10)
+                return -1;
+
+            return 0;
+        }
+
+        public bool Equals(Converter other)
+        {
+            if (other == null)
+                return false;
+            
+            return _num * other._denom == other._num * _denom;
+        }
+
+        public static bool ToConverter(string conv, out Converter obj)
+        {
+
+            obj = null;
+            string[] parts = conv.Split('/');
+
+            if (parts.Length == 2 && Int32.TryParse(parts[0], out int part1) && Int32.TryParse(parts[1], out int part2) && part1 != 0 && part2 > 0)
+            {
+                obj = new Converter(Convert.ToInt32(parts[0]), Convert.ToInt32(parts[1]));
+                return true;
+            }
+            else if (parts.Length == 1)
+            {
+                parts = conv.Split(',');
+                if (parts.Length == 2 && Int32.TryParse(parts[0], out part1))
+                {
+                    for (int i = 0; i < parts[1].Length; i++)
+                    {
+                        if (parts[0][0] == '-')
+                        {
+                            part1 = part1 * 10 - (int)Char.GetNumericValue(parts[1][i]);
+                        }
+                        else
+                        {
+                            part1 = part1 * 10 + (int)Char.GetNumericValue(parts[1][i]);
+                        }
+                    }
+                    obj = new Converter(part1, (int)Math.Pow(10, parts[1].Length));
+                    return true;
+                }
+                else if (parts.Length == 1 && Int32.TryParse(parts[0], out part1))
+                {
+                    obj = new Converter(part1, 1);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public static explicit operator double(Converter value)
+        {
+            return value._num / (double)value._denom;
+        }
+
+        public static implicit operator int(Converter value)
+        {
+            return value._num / value._denom;
+        }
+
+    }
+}
---
+++ Lab7/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace Lab7
+{
+    class Program
+    {
+        public static void Menu()
+        {
+            Console.Clear();
+
+            Console.WriteLine("1. Сложение(+)\n2. Вычитание(-)" +
+            "\n3. Умножение(*)\n4. Деление(/)" +
+            "\n5. Проверка на знак < \n6. Проверка на знак  >" +
+            "\n7. Отсортировать(в массиве с 2/3 и 3/4) \n8. Проверка на равенство чисел" +
+            "\n9. Преобразовать в double \n0. Выход");
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("Введите 1-ую дробь(x/y): ");
+            string number = Console.ReadLine();
+            Console.WriteLine("Введите 2-ую дробь(x/y): ");
+            string number2 = Console.ReadLine();
+
+            Converter.ToConverter(number, out Converter firstnum);
+            Converter.ToConverter(number2, out Converter secondnum);
+
+            Converter forcase1 = new Converter(2, 3);
+            Converter forcase2 = new Converter(3, 4);
+
+            Converter[] mass = new Converter[] { firstnum, secondnum, forcase1, forcase2 };
+            Menu();
+
+            int choose;
+            do
+            {
+                choose = Console.Read() - 48;
+                Console.Write("\b");
+
+                switch (choose)
+                {
+                    case 1:
+                        Console.WriteLine($"Сумма: {firstnum + secondnum}");
+                        break;
+
+                    case 2:
+                        Console.WriteLine($"Разность: {firstnum - secondnum}");
+                        break;
+
+                    case 3:
+                        Console.WriteLine($"Произведение: {firstnum * secondnum}");
+                        break;
+
+                    case 4:
+                        Console.WriteLine($"Частное: {firstnum / secondnum}");
+                        break;
+
+                    case 5:
+                        Console.WriteLine($"{firstnum} < {secondnum} = {firstnum < secondnum}");
+                        break;
+
+                    case 6:
+                        Console.WriteLine($"{firstnum} > {secondnum} = {firstnum > secondnum}");
+                        break;
+
+                    case 7:
+                        Array.Sort(mass);
+                        foreach (var i in mass)
+                            Console.WriteLine($"{i}");
+                        break;
+
+                    case 8:
+                        Console.WriteLine($"Числа равны: {firstnum.Equals(secondnum)}");
+                        break;
+
+                    case 9:
+                        double one = (double)firstnum;
+                        Console.WriteLine($"{one} : {firstnum}");
+                        double two = (double)secondnum;
+                        Console.WriteLine($"{two} : {secondnum}");
+                        break;
+
+                    case 0:
+                        return;
+
+                    default:
+                        Console.Write("\b");
+                        break;
+                }
+            } while (true);
+        }
+    }
+}
